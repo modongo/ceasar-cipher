aa9fb68fbfda129882a65e28eab78025e7ffdcfe
Michael Odongo <mikodongo@gmail.com>
2019-05-18 09:18:58 +0300

    Updated Catch Try handler

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,14 @@ public class App {
             int key;
             try {
                 //Section for User entry for encode
-                System.out.println("Enter a Plaintext you want to encrypt: ");
-                String plaintext = mSc.readLine();
+                    System.out.println("                                       ");
+                   System.out.println("                                       ");
+                    System.out.println("<<<< Enter a Plaintext you want to encrypt: >>>>  ");
+                    String plaintext = mSc.readLine();
+
                do {
-                   System.out.println("Enter encrypted key: ");
+                   System.out.println("                                       ");
+                   System.out.println("<<<<  Enter encrypted key: >>>>>  ");
                    String mKey = mSc.readLine();
                    key = Integer.parseInt(mKey);
                } while ( key < 0 || key > 25);
@@ -30,17 +34,24 @@ public class App {
                 //display the crypt
 
                 if (encode.getCrypt().equals(plaintext)) {
-                    System.out.println(" No Encryption done: " + encode.getCrypt());
+                    System.out.println("                                       ");
+                    System.out.println("<<<    No Encryption done:   >>>  " + encode.getCrypt());
+                    System.out.println("                                       ");
                 } else {
-                    System.out.println(" The Crypt is: " + encode.getCrypt());
+                    System.out.println("<<<    The Crypt is :   >>>  " + encode.getCrypt());
+
                 }
 
 
                 /**Section for User entry for decode**/
+                System.out.println("                                       ");
+                System.out.println("               LET'S NOW DECRYPT                        ");
                 System.out.println("Enter an encrypted message you want to decrypt: ");
+                System.out.println("                                       ");
                 String ciphertext = mSc.readLine();
 
                do {
+                   System.out.println("                                       ");
                    System.out.println("Enter decryption key: ");
                     String mKey = mSc.readLine();
                    key = Integer.parseInt(mKey);
@@ -51,12 +62,25 @@ public class App {
                 decode.setKey(key);
                 decode.doDecode(ciphertext, key);
                 if (decode.getmDecrypted_Message().equals(ciphertext)) {
-                    System.out.println("Your message was never encrypted. ");
+                    System.out.println("                                       ");
+                    System.out.println("<<< Sorry, Your original message must have been just numbers. It was never.>>>");
+                    System.out.println("                                       ");
+                    System.out.println("                Thanks & Bye           ");
+                    runner=false;
+                    break;
                 } else {
+                    System.out.println("                                       ");
+                    System.out.println("                                       ");
                     System.out.println("The original Message was: " + decode.getmDecrypted_Message());
+                    System.out.println("                                       ");
+                    System.out.println("                                       ");
+                    System.out.println("                 Thanks & Bye!!                      ");
+                    runner=false;
+                    break;
                 }
             } catch (NumberFormatException e){
-                System.out.println("Invalid. The key must be [1-25]");
+                System.out.println("!!! Invalid!!!  The key must be [1-25]. ");
+                System.out.println("               Start all over again                        ");
             }
 
         }
